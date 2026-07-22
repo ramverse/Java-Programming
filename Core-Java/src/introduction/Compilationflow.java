7de9d708c2f451437d777f2d369c7d9051045928
ramverse <ramkrishna.tech06@gmail.com>
2026-07-22 09:45:36 +0530

    Add Introduction package with basic Java programs

---
+++ Core-Java/src/introduction/Compilationflow.java
@@ -0,0 +1,30 @@
+package introduction;
+
+public class Compilationflow {
+
+    public static void main(){
+
+        System.out.println("Java socure code");
+        System.out.println("Complier");
+        System.out.println("Byte code");
+        System.out.println("JVM");
+        System.out.println("Output");
+
+
+// \n helps to print the sentence or word to next line
+        System.out.println("Welcome\nTo Java");
+
+//  \t helps to print the sentence or word with space
+        System.out.println("Java\tProgramming");
+
+//     println() helps to print the sentence or word to next line
+
+//       print() helps to print the sentence or word to the same line with out space
+        System.out.print("Understanding java");
+        System.out.println("basics");
+
+        System.out.print("concept");
+
+
+    }
+}
---
+++ Core-Java/src/introduction/Hello.java
@@ -0,0 +1,8 @@
+package introduction;
+
+public class Hello {
+   public static void main(){
+       System.out.print("RAM RAM");
+   }
+
+}
