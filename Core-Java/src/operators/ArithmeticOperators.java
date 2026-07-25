dc27dc7c05eab7c5094002eb156b9b0bd7ca8710
ramverse <ramkrishna.tech06@gmail.com>
2026-07-25 09:40:53 +0530

    understood and practiced the arithmetic and assignment operators

---
+++ Core-Java/src/operators/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class ArithmeticOperators {
+    public static void main(){
+
+        int a=50;
+        int b=20;
+
+        System.out.println("addition operation a + b ="+ (a+b));
+        System.out.println("substraction operation a-b ="+ (a-b));
+        System.out.println("multiply operation a  + b ="+ (a*b));
+        System.out.println("division operation  a + b ="+ (a/b));
+    }
+
+
+
+
+}
---
+++ Core-Java/src/operators/Assignmentoperator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class Assignmentoperator {
+
+    public static void main(){
+
+        int marks = 80;
+
+        marks += 10; // marks =90
+        marks -= 10; // marks=80
+        marks /= 2;  // marks=40
+        marks += 60; // marks=100
+
+        System.out.println("marks = "+marks);
+
+
+    }
+
+}
