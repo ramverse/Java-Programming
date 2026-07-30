0642fa8ad94b745601bc050263013291c9163a3f
ramverse <ramkrishna.tech06@gmail.com>
2026-07-30 22:00:04 +0530

    Add Scanner related programs

---
+++ Core-Java/src/inputoutput/ScannerIntroduction.java
@@ -0,0 +1,17 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class ScannerIntroduction {
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Scanner object created successfully.");
+
+        sc.close();
+    }
+}
