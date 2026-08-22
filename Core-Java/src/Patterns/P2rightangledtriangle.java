71dc06f68788b9bb50ee5c7e41047a2d20b52d5a
ramverse <ramkrishna.tech06@gmail.com>
2026-08-22 19:35:25 +0530

    mastering loops using patterns

---
+++ Core-Java/src/Patterns/P2rightangledtriangle.java
@@ -0,0 +1,35 @@
+package Patterns;
+
+public class P2rightangledtriangle {
+
+    public static void main(){
+
+//        for(int i=0;i<6;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print("* ");
+//            }
+//
+//            System.out.println();
+//        }
+
+
+//        inverted rignt angled triangle
+
+//        for(int i=6;i>=1;i--){
+//            for(int j=i;j>=1;j--){
+//                System.out.print("* ");
+//            }
+//
+//            System.out.println();
+//        }
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+
+}
